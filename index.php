db0290153221f10c5776f3b7657d1a8f2298377a
Vensz <gamercrit@gmail.com>
2020-09-19 11:25:20 +0300

    adding index.php to repo

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-    echo "Hello World !";
+    echo "Hello World ! <br>";
+
+    $username = "Vnesz";
+    echo $username."<br>";
+    $age = 22;
+    echo $age."<br>";
+    $bool = 1123;
+    print_r($bool."<br><br>"); 
+    $array = [true, false, 3689, "true"];
+    foreach ($array as $arrval)
+        {
+            echo $arrval." | ";
+        }
+    
