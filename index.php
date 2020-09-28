968597b35d48b3472c7df74987e1f1b56589583a
HristiannaM <hrisi_razgrad@abv.bg>
2020-09-28 08:27:34 +0300

    if and switch

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
+    // echo "Hello World ! <br>";
 
-    echo "Hello World ! <br>";
-
-    $username = "Vnesz";
-    echo $username."<br>";
-    $age = 22;
-    echo $age."<br>";
-    $bool = 1123;
-    print_r($bool."<br><br>"); 
-    $array = [true, false, 3689, "true"];
-    foreach ($array as $arrval)
-        {
-            echo $arrval." | ";
-        }
+    // $username = "Vnesz";
+    // echo $username."<br>";
+    // $age = 22;
+    // echo $age."<br>";
+    // $bool = 1123;
+    // print_r($bool."<br><br>"); 
+    // $array = [true, false, 3689, "true"];
+    // foreach ($array as $arrval)
+    //     {
+    //         echo $arrval." | ";
+    //     }
     
+    $classStartAt = 420;
+    $classStartAt2 = 500;
+    // if($classStartAt > $classStartAt2)
+    // {
+    //     echo "Its too early anyway, I am not coming";
+    // }
+    // else 
+    // {
+    //     echo "WTF ?!";
+    // }
+
+    switch($classStartAt){
+        case $classStartAt > $classStartAt2 : 
+            echo "case 1";
+            break;
+        case $classStartAt < $classStartAt2:
+            echo "case 2";
+            break;
+        default : 
+            echo "else text";
+    }
