22f1164bbcb1acf1a236039357633d176fd2b1e1
HristiannaM <hrisi_razgrad@abv.bg>
2020-09-28 09:05:52 +0300

    adding class and html

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     //         echo $arrval." | ";
     //     }
     
-    $classStartAt = 420;
-    $classStartAt2 = 500;
+    // $classStartAt = 420;
+    // $classStartAt2 = 500;
     // if($classStartAt > $classStartAt2)
     // {
     //     echo "Its too early anyway, I am not coming";
@@ -24,13 +24,39 @@
     //     echo "WTF ?!";
     // }
 
-    switch($classStartAt){
-        case $classStartAt > $classStartAt2 : 
-            echo "case 1";
-            break;
-        case $classStartAt < $classStartAt2:
-            echo "case 2";
-            break;
-        default : 
-            echo "else text";
+    // switch($classStartAt){
+    //     case $classStartAt > $classStartAt2 : 
+    //         echo "case 1";
+    //         break;
+    //     case $classStartAt < $classStartAt2:
+    //         echo "case 2";
+    //         break;
+    //     default : 
+    //         echo "else text";
+    // }
+
+    // if (isset($_POST) && !empty($_POST)) {
+    //     if ($_POST["username"] === "Venzy" && $_POST["password"] == "1234") {
+    //         echo "Logged in";
+    //     } else {
+    //         echo "Unauthorized";
+    //     }
+    // }
+
+    class Person
+    {
+        private $username; 
+    
+        public function __construct ($myName)
+        {      
+            $this->username = $myName;
+        }     
+        public function printUsername() 
+        {
+            echo "This is ivan's username: ".$this->username;
+        }
     }
+
+    
+$petur = new Person("Petur");
+$petur->printUsername();
